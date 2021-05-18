b72beac2405ea836ec91ad22a87dbd1c5523c7e9
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-18 10:54:39 +0530

    Update SeleniumTest.java

--- src/test/java/hello1/SeleniumTest.java
+++ src/test/java/hello1/SeleniumTest.java
@@ -17,10 +17,11 @@ public class SeleniumTest {
 //        options.addArguments("headless");
 options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
-       // driver.get("https://www.google.com");
-        driver.get("http://65.2.108.33:8080/");
+        //driver.get("https://www.google.com");
+        driver.get("http://flogile.com/");
+       // driver.get("http://65.2.108.33:8080/");
         System.out.println(driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Welcome to Flogile Technologies!"));
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Flogile Technologies"));
         //Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
     }
 
