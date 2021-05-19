75697ebfe006d441b5d4c3f3407e30e579fd0e98
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-19 16:13:27 +0530

    Update SeleniumTest.java

--- src/test/java/hello1/SeleniumTest.java
+++ src/test/java/hello1/SeleniumTest.java
@@ -18,7 +18,7 @@ public class SeleniumTest {
         //WebDriver driver =new FirefoxDriver();
          WebDriver driver = new ChromeDriver();
 //        options.addArguments("headless");
-options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
+//options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
        
         //driver = new ChromeDriver(options);
         //driver.get("https://www.google.com");
