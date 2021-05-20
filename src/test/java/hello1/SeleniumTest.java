585b4dfa6721bafa1c7dbd9a88c6653bbc08f209
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-20 11:53:20 +0530

    Update SeleniumTest.java

--- src/test/java/hello1/SeleniumTest.java
+++ src/test/java/hello1/SeleniumTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+//import org.testing.Assert;
+import org.testing.annotations.test;
 
 public class SeleniumTest {
 
